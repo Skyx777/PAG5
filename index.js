c72f5f373a3d81d36a01efa3904a1e90b07f9713
Skyx777 <113266792+Skyx777@users.noreply.github.com>
2022-12-15 13:35:41 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+alert("Digite a seguir seus números")
+let x= prompt('Digite sua resposta')
+   if(x==="6024"){
+       alert('Você conseguiu!!')
+   }
+
+   else if(x!=='6024'){
+       alert('Você errou, volte e tente denovo')
+   }
+   
